b1492eef5971bedb67db6fdc7ce2cebdc0be7deb
10555019 <10555019@kec309-14.LEARNER.STUDENT>
2013-11-25 12:50:12 +0800

    added Db.java - Kelvin

---
+++ CareMRS/src/Db.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+
+public class Db {
+	
+	private LinkedList<Patient> patient = new LinkedList<Patient>();
+	private LinkedList<Doctor> doctor = new LinkedList<Doctor>();
+	private Clinic clinic = new Clinic();
+
+}
