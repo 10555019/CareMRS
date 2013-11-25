b94e15afdcd321750b06d53b837da7373377badd
10555019 <hkuspacejava@outlook.com>
2013-11-25 14:09:57 +0800

    added method

--- CareMRS/src/Db.java
+++ CareMRS/src/Db.java
@@ -6,5 +6,17 @@ public class Db {
 	private LinkedList<Patient> patient = new LinkedList<Patient>();
 	private LinkedList<Doctor> doctor = new LinkedList<Doctor>();
 	private Clinic clinic = new Clinic();
-
+	
+	public Db(){
+		
+	}
+	
+	public void addPatient(Patient patient){
+		this.patient.add(patient);
+	}
+	
+	public void addDoctor(Doctor doctor){
+		this.doctor.add(doctor);
+	}
+	
 }
