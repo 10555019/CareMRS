94555d336785d377699a00b6f2c43fbdff5960fb
10555019 <10555019@kec910-01.LEARNER.STUDENT>
2013-11-26 14:52:45 +0800

    changed to static

--- CareMRS/src/Db.java
+++ CareMRS/src/Db.java
@@ -3,28 +3,28 @@ import java.util.LinkedList;
 
 public class Db {
 	
-	private LinkedList<Patient> patient = new LinkedList<Patient>();
-	private LinkedList<Doctor> doctor = new LinkedList<Doctor>();
-	private Clinic clinic = new Clinic();
+	private static LinkedList<Patient> l_patient = new LinkedList<Patient>();
+	private static LinkedList<Doctor> l_doctor = new LinkedList<Doctor>();
+	private static Clinic clinic = new Clinic();
 	
 	public Db(){
 		
 	}
 	
-	public void addPatient(Patient patient){
-		this.patient.add(patient);
+	public static void addPatient(Patient patient){
+		l_patient.add(patient);
 	}
 	
-	public void addDoctor(Doctor doctor){
-		this.doctor.add(doctor);
+	public static void addDoctor(Doctor doctor){
+		l_doctor.add(doctor);
 	}
 	
-	public Patient getPatient(int index){
-		return patient.get(index);
+	public static Patient getPatient(int index){
+		return l_patient.get(index);
 	}
 	
-	public Doctor getDoctor(int index){
-		return doctor.get(index);
+	public static Doctor getDoctor(int index){
+		return l_doctor.get(index);
 	}
 	
 }
