b5f16b816c030400b5078eda111d61df3aca2146
tony <tonyfatchan@gmail.com>
2013-11-26 17:29:51 +0800

    .....

--- CareMRS/src/DBooking.java
+++ CareMRS/src/DBooking.java
@@ -4,4 +4,14 @@ import java.util.Date;
 public class DBooking {
 	private Date time;
 	private int patientID;
+	public DBooking(int patientID){
+		this.patientID = patientID;
+	}
+	public changeTime(){
+		
+	}
+	public showBooking(){
+		
+	}
+	public 
 }
--- CareMRS/src/PBooking.java
+++ CareMRS/src/PBooking.java
@@ -4,4 +4,16 @@ import java.util.Date;
 public class PBooking {
 	private Date time;
 	private int patientID;
+	public PBooking(int patientID){
+		this.patientID = patientID;
+	}
+	public ShowBooking(){
+		
+	}
+	public PrintBooking(){
+		
+	}
+	public CreateBooking(){
+		
+	}
 }
