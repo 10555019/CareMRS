10e4dbbbe0354070d5fdbccef649a44816f9d1bf
10555019 <hkuspacejava@outlook.com>
2013-11-30 14:21:03 +0800

    deleted modifier "static" - Kelvin

--- CareMRS/src/Db.java
+++ CareMRS/src/Db.java
@@ -3,33 +3,33 @@ import java.util.LinkedList;
 
 public class Db {
 	
-	private static LinkedList<Patient> l_patient = new LinkedList<Patient>();
-	private static LinkedList<Doctor> l_doctor = new LinkedList<Doctor>();
-	private static Clinic clinic = new Clinic();
+	private LinkedList<Patient> patient = new LinkedList<Patient>();
+	private LinkedList<Doctor> doctor = new LinkedList<Doctor>();
+	private Clinic clinic = new Clinic();
 	
 	public Db(){
 		
 	}
 	
-	public static void addPatient(Patient patient){
-		l_patient.add(patient);
+	public void addPatient(Patient patient){
+		this.patient.add(patient);
 	}
 	
-	public static void addDoctor(Doctor doctor){
-		l_doctor.add(doctor);
+	public void addDoctor(Doctor doctor){
+		this.doctor.add(doctor);
 	}
 	
-	public static Patient getPatient(int index){
-		return l_patient.get(index);
+	public Patient getPatient(int index){
+		return patient.get(index);
 	}
 	
-	public static int getPatientSize(){
-		return l_patient.size();
+	public int getPatientSize(){
+		return patient.size();
 	}
 	
-	public static Doctor getDoctor(int index){
+	public Doctor getDoctor(int index){
 		try{
-			return l_doctor.get(index);
+			return doctor.get(index);
 		} catch (IndexOutOfBoundsException e){
 		}
 		return null;
