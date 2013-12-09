fa694288fcf3ca79acc3440250037c40ccaa7cc2
tony <tonyfatchan@gmail.com>
2013-12-10 02:49:19 +0800

    minner change

--- CareMRS/src/FunBooking.java
+++ CareMRS/src/FunBooking.java
@@ -1,18 +1,19 @@
 import java.io.Serializable;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 
 public class FunBooking implements Serializable {
-	
+	private LinkedList<Booking> booking = new LinkedList<booking>();
 	public void CancelBooking(String ID, GregorianCalendar date){
 		for(int i=0; i<booking.size();i++){
-		if ()
+		//remove
 	}
 	public void ShowBooking(){
-	
+		
 	}
-	public void Createbooking(){
-	
+	public void Createbooking(String ID, GregorianCalendar date){
+		//add list
 	}
 	public void Checkoverlap(){
 		for (int i=0; i<booking.size();i++)
