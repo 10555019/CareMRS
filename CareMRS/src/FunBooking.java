8b5089b8a87a67e15b8be262c46dc8fdd032f52e
tony <tonyfatchan@gmail.com>
2013-12-10 11:08:49 +0800

    small change

--- CareMRS/src/FunBooking.java
+++ CareMRS/src/FunBooking.java
@@ -4,20 +4,30 @@ import java.util.LinkedList;
 
 
 public class FunBooking implements Serializable {
-	private LinkedList<Booking> booking = new LinkedList<booking>();
-	public void CancelBooking(String ID, GregorianCalendar date){
-		for(int i=0; i<booking.size();i++){
-		//remove
-	}
-	public void ShowBooking(){
+	private LinkedList<Booking> booking = new LinkedList<Booking>();
+	private String DoctorID;
+	private String PID;
+	//private 
+	public FunBooking(String DoctorID, String PID){
+		this.DoctorID = DoctorID;
+		this.PID = PID;
 		
 	}
-	public void Createbooking(String ID, GregorianCalendar date){
+	public void cancelBooking(String ID, GregorianCalendar date){
+		for(int i=0; i<booking.size();i++){//remove
+		
+		}
+	}
+	public void showBooking(){//print out list
+		
+	}
+	public void createbooking(String ID, GregorianCalendar date){
 		//add list
+		
 	}
-	public void Checkoverlap(){
-		for (int i=0; i<booking.size();i++)
-	}
+	public void checkoverlap(){
+		for (int i=0; i<booking.size();i++){//search and check
 
+		}
 	}
 }
